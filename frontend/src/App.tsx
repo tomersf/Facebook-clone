20afc1e2bce0a226b719dc56d8510ac9b1b59e17
Tomer Shafir <tomershafir@Tomers-MacBook-Pro-5.local>
2022-07-19 15:29:13 +0300

    add home/login/profile pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <div>welcome to frontend</div>;
-}
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
