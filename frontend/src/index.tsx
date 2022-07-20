424ca967e85bf4ab196945cf7140992ad1559ba8
Tomer Shafir <tomershafir@Tomers-MacBook-Pro-5.local>
2022-07-20 11:10:12 +0300

    add redux

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+
+const store = configureStore({ reducer: rootReducer });
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
